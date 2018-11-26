97ab8c244af959d53e6b6930d3e1a6444830c26e
David Baruchel <dbaruchel@gmail.com>
2018-11-26 10:57:19 +0100

    switchTo et togglePlayer

--- client/main.js
+++ client/main.js
@@ -123,6 +123,45 @@ if (Meteor.isClient) {
     YT.load();
 }
 
+togglePlayer = function (player) {
+	if (player.getPlayerState() == 1) {
+		player.pauseVideo();
+	}
+	else {
+		player.playVideo();
+	}
+};
+
+switchTo = function (number) {
+	if (number == 1) {
+		player2.pauseVideo();
+		player3.pauseVideo();
+		player4.pauseVideo();
+		player1.playVideo();
+	}
+	else if (number == 2) {
+		player1.pauseVideo();
+		player3.pauseVideo();
+		player4.pauseVideo();
+		player2.playVideo();
+	}
+	else if (number == 3) {
+		player1.pauseVideo();
+		player2.pauseVideo();
+		player4.pauseVideo();
+		player3.playVideo();
+	}
+	else if (number == 4) {
+		player1.pauseVideo();
+		player2.pauseVideo();
+		player3.pauseVideo();
+		player4.playVideo();
+	}
+	else {
+		console.log('switchTo bloup');
+	}
+}
+
 
 Template.hello.onRendered(function helloOnCreated() {
   	// counter starts at 0
@@ -155,31 +194,35 @@ Template.hello.onRendered(function helloOnCreated() {
 
   	    if ( prediction.x < ww/2 && prediction.y < wh/2 ) {
   	    	$("#target-2, #target-3, #target-4").removeClass("gazed");
-  	    	$("#target-2 .gif, #target-3 .gif, #target-4 .gif").gifplayer('stop');
-  	    	$("#target-1 .gif").gifplayer('play');
+  	    	// $("#target-2 .gif, #target-3 .gif, #target-4 .gif").gifplayer('stop');
+  	    	// $("#target-1 .gif").gifplayer('play');
   	    	$("#target-1").addClass("gazed");
+  	    	switchTo(1);
 
   	    }
   	    else if ( prediction.x > ww/2 && prediction.y < wh/2  ){
   	    	$("#target-1, #target-3, #target-4").removeClass("gazed");
-  	    	$("#target-1 .gif, #target-3 .gif, #target-4 .gif").gifplayer('stop');
-  	    	$("#target-2 .gif").gifplayer('play');
+  	    	// $("#target-1 .gif, #target-3 .gif, #target-4 .gif").gifplayer('stop');
+  	    	// $("#target-2 .gif").gifplayer('play');
   	    	$("#target-2").addClass("gazed");
+  	    	switchTo(2);
   	    }
   	    else if ( prediction.x > ww/2 && prediction.y > wh/2  ){
   	    	$("#target-1, #target-2, #target-4").removeClass("gazed");
-  	    	$("#target-1 .gif, #target-2 .gif, #target-4 .gif").gifplayer('stop');
-  	    	$("#target-3 .gif").gifplayer('play');
+  	    	// $("#target-1 .gif, #target-2 .gif, #target-4 .gif").gifplayer('stop');
+  	    	// $("#target-3 .gif").gifplayer('play');
   	    	$("#target-3").addClass("gazed");
+  	    	switchTo(3);
   	    }
   	    else if ( prediction.x < ww/2 && prediction.y > wh/2  ) {
   	    	$("#target-1, #target-2, #target-3").removeClass("gazed");
-  	    	$("#target-2 .gif, #target-3 .gif, #target-1 .gif").gifplayer('stop');
-  	    	$("#target-4 .gif").gifplayer('play');
+  	    	// $("#target-2 .gif, #target-3 .gif, #target-1 .gif").gifplayer('stop');
+  	    	// $("#target-4 .gif").gifplayer('play');
   	    	$("#target-4").addClass("gazed");
+  	    	switchTo(4);
   	    }
   	    else {
-  	    	console.log('ouch!');
+  	    	console.log('prediction ouch!');
   	    }
   	}, 1000);
 
@@ -204,43 +247,22 @@ Template.hello.events({
      // $(".gif").freezeframe();
      // $(".gif").gifplayer('stop');
   },
-  'click #target-1': function(e, i) {
-  	player1.mute();
-  	if (player1.getPlayerState() == 1) {
-  		player1.pauseVideo();
-  	}
-  	else {
-  		player1.playVideo();
-  	}
-
-  },
-  'click #target-2': function(e, i) {
-  	player2.mute();
-  	if (player2.getPlayerState() == 1) {
-  		player2.pauseVideo();
-  	}
-  	else {
-  		player2.playVideo();
-  	}
-  },
-  'click #target-3': function(e, i) {
-  	player3.mute();
-  	if (player3.getPlayerState() == 1) {
-  		player3.pauseVideo();
-  	}
-  	else {
-  		player3.playVideo();
-  	}
-  },
-  'click #target-4': function(e, i) {
-  	player4.mute();
-  	if (player4.getPlayerState() == 1) {
-  		player4.pauseVideo();
-  	}
-  	else {
-  		player4.playVideo();
-  	}
-  },
+  // 'click #target-1': function(e, i) {
+  // 	player1.mute();
+  // 	switchTo(1);
+  // },
+  // 'click #target-2': function(e, i) {
+  // 	player2.mute();
+  // 	switchTo(2);
+  // },
+  // 'click #target-3': function(e, i) {
+  // 	player3.mute();
+  // 	switchTo(3);
+  // },
+  // 'click #target-4': function(e, i) {
+  // 	player4.mute();
+  // 	switchTo(4);
+  // },
   'click #webgazer-pause': function(e, i) {
   	webgazer.pause();
   },
